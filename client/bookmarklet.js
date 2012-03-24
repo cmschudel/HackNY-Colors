7f66e770fa70db3e05bec8778fdd796da8f775c8
Sid Nair <ssn2114@columbia.edu>
2012-03-24 17:38:27 -0400

    Restructure dir, add sample bookmarklet code

---
+++ client/bookmarklet.js
@@ -0,0 +1,37 @@
+function foo() { 
+
+  var SEARCH_URL = "http://127.0.0.1:5000/search";
+
+  // Add jQuery - this introduces unncessary overhead, but it's convenient.
+  var s = document.createElement('script');
+  s.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+  document.body.appendChild(s);
+
+  // Get colors from the page.
+  function getColors() {
+    // TODO
+    return 'blue,red';
+  }
+
+  function sendColors() {
+    $.get(SEARCH_URL, {
+      colors: getColors()
+    }, function() {
+      console.log('in callback');
+    });
+  }
+
+  // This calls the callback after jQuery has loaded.
+  function onJQReady(cb) {
+    if (window.$) {
+      cb();
+    } else {
+      setTimeout(function() {
+        onJQReady(cb);
+      }, 10);
+    }
+  }
+
+  onJQReady(sendColors);
+
+ }
